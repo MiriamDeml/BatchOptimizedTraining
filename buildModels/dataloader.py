235fb716824e15499ec8bcc4273ba392defc58d2
MiriamDeml <demlmiri@fim.uni-passau.de>
2020-06-01 08:28:02 +0200

    Create dataloader.py

---
+++ buildModels/dataloader.py
@@ -0,0 +1,286 @@
+import torch
+import torchvision.transforms as transforms
+import random
+
+import os
+import torchvision.datasets as datasets
+
+# Code by Li et al. (2018): https://github.com/tomgoldstein/loss-landscape/blob/master/cifar10/dataloader.py
+# Code by Hoffer et al. (2017): https://github.com/eladhoffer/bigBatch
+
+__DATASETS_DEFAULT_PATH = '/media/ssd/Datasets/'
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.float().topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+        
+def get_dataset(name, split='train', transform=None,
+                target_transform=None, download=True, datasets_path=__DATASETS_DEFAULT_PATH):
+    train = (split == 'train')
+    root = os.path.join(datasets_path, name)
+    if name == 'CIFAR10':
+        return datasets.CIFAR10(root=root,
+                                train=train,
+                                transform=transform,
+                                target_transform=target_transform,
+                                download=download)
+    elif name == 'CIFAR100':
+        return datasets.CIFAR100(root=root,
+                                 train=train,
+                                 transform=transform,
+                                 target_transform=target_transform,
+                                 download=download)
+    elif name == 'mnist':
+        return datasets.MNIST(root=root,
+                              train=train,
+                              transform=transform,
+                              target_transform=target_transform,
+                              download=download)
+    elif name == 'fashionmnist':
+        return datasets.FashionMNIST(root=root,
+                              train=train,
+                              transform=transform,
+                              target_transform=target_transform,
+                              download=download)
+    elif name == 'stl10':
+        return datasets.STL10(root=root,
+                              split=split,
+                              transform=transform,
+                              target_transform=target_transform,
+                              download=download)
+    elif name == 'imagenet':
+        if train:
+            root = os.path.join(root, 'train')
+        else:
+            root = os.path.join(root, 'val')
+        return datasets.ImageFolder(root=root,
+                                    transform=transform,
+                                    target_transform=target_transform)
+    
+
+__imagenet_stats = {'mean': [0.485, 0.456, 0.406],
+                   'std': [0.229, 0.224, 0.225]}
+
+__imagenet_pca = {
+    'eigval': torch.Tensor([0.2175, 0.0188, 0.0045]),
+    'eigvec': torch.Tensor([
+        [-0.5675,  0.7192,  0.4009],
+        [-0.5808, -0.0045, -0.8140],
+        [-0.5836, -0.6948,  0.4203],
+    ])
+}
+
+
+def scale_crop(input_size, scale_size=None, normalize=__imagenet_stats):
+    t_list = [
+        transforms.CenterCrop(input_size),
+        transforms.ToTensor(),
+        transforms.Normalize(**normalize),
+    ]
+    if scale_size != input_size:
+        t_list = [transforms.Scale(scale_size)] + t_list
+
+    return transforms.Compose(t_list)
+
+
+def scale_random_crop(input_size, scale_size=None, normalize=__imagenet_stats):
+    t_list = [
+        transforms.RandomCrop(input_size),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(**normalize),
+    ]
+    if scale_size != input_size:
+        t_list = [transforms.Scale(scale_size)] + t_list
+
+    return transforms.Compose(t_list)
+
+
+def pad_random_crop(input_size, scale_size=None, normalize=__imagenet_stats, fill=0):
+    padding = int((scale_size - input_size) / 2)
+    return transforms.Compose([
+        transforms.Pad(padding, fill=fill),
+        transforms.RandomCrop(input_size),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(**normalize),
+    ])
+
+
+def inception_preproccess(input_size, normalize=__imagenet_stats):
+    return transforms.Compose([
+        transforms.RandomSizedCrop(input_size),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(**normalize)
+    ])
+def inception_color_preproccess(input_size, normalize=__imagenet_stats):
+    return transforms.Compose([
+        transforms.RandomSizedCrop(input_size),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        ColorJitter(
+            brightness=0.4,
+            contrast=0.4,
+            saturation=0.4,
+        ),
+        Lighting(0.1, __imagenet_pca['eigval'], __imagenet_pca['eigvec']),
+        transforms.Normalize(**normalize)
+    ])
+
+
+def get_transform(name='imagenet', input_size=None,
+                  scale_size=None, normalize=None, augment=True):
+    normalize = normalize or __imagenet_stats
+    if name == 'imagenet':
+        scale_size = scale_size or 256
+        input_size = input_size or 224
+        if augment:
+            return inception_preproccess(input_size, normalize=normalize)
+        else:
+            return scale_crop(input_size=input_size,
+                              scale_size=scale_size, normalize=normalize)
+    elif 'CIFAR' in name:
+        input_size = input_size or 32
+        if augment:
+            scale_size = scale_size or 40
+            return pad_random_crop(input_size, scale_size=scale_size,
+                                   normalize=normalize, fill=127)
+        else:
+            scale_size = scale_size or 32
+            return scale_crop(input_size=input_size,
+                              scale_size=scale_size, normalize=normalize)
+    elif name == 'mnist':
+        normalize = {'mean': [0.5], 'std': [0.5]}
+        input_size = input_size or 28
+        if augment:
+            scale_size = scale_size or 32
+            return pad_random_crop(input_size, scale_size=scale_size,
+                                   normalize=normalize)
+        else:
+            scale_size = scale_size or 28
+            return scale_crop(input_size=input_size,
+                              scale_size=scale_size, normalize=normalize)
+
+
+class Lighting(object):
+    """Lighting noise(AlexNet - style PCA - based noise)"""
+
+    def __init__(self, alphastd, eigval, eigvec):
+        self.alphastd = alphastd
+        self.eigval = eigval
+        self.eigvec = eigvec
+
+    def __call__(self, img):
+        if self.alphastd == 0:
+            return img
+
+        alpha = img.new().resize_(3).normal_(0, self.alphastd)
+        rgb = self.eigvec.type_as(img).clone()\
+            .mul(alpha.view(1, 3).expand(3, 3))\
+            .mul(self.eigval.view(1, 3).expand(3, 3))\
+            .sum(1).squeeze()
+
+        return img.add(rgb.view(3, 1, 1).expand_as(img))
+
+
+class Grayscale(object):
+
+    def __call__(self, img):
+        gs = img.clone()
+        gs[0].mul_(0.299).add_(0.587, gs[1]).add_(0.114, gs[2])
+        gs[1].copy_(gs[0])
+        gs[2].copy_(gs[0])
+        return gs
+
+
+class Saturation(object):
+
+    def __init__(self, var):
+        self.var = var
+
+    def __call__(self, img):
+        gs = Grayscale()(img)
+        alpha = random.uniform(0, self.var)
+        return img.lerp(gs, alpha)
+
+
+class Brightness(object):
+
+    def __init__(self, var):
+        self.var = var
+
+    def __call__(self, img):
+        gs = img.new().resize_as_(img).zero_()
+        alpha = random.uniform(0, self.var)
+        return img.lerp(gs, alpha)
+
+
+class Contrast(object):
+
+    def __init__(self, var):
+        self.var = var
+
+    def __call__(self, img):
+        gs = Grayscale()(img)
+        gs.fill_(gs.mean())
+        alpha = random.uniform(0, self.var)
+        return img.lerp(gs, alpha)
+
+
+class RandomOrder(object):
+    """ Composes several transforms together in random order.
+    """
+
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, img):
+        if self.transforms is None:
+            return img
+        order = torch.randperm(len(self.transforms))
+        for i in order:
+            img = self.transforms[i](img)
+        return img
+
+
+class ColorJitter(RandomOrder):
+
+    def __init__(self, brightness=0.4, contrast=0.4, saturation=0.4):
+        self.transforms = []
+        if brightness != 0:
+            self.transforms.append(Brightness(brightness))
+        if contrast != 0:
+            self.transforms.append(Contrast(contrast))
+        if saturation != 0:
+            self.transforms.append(Saturation(saturation))
+
