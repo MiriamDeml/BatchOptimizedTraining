55d2547ce4c3a956af5117d93fb3cefaafabb298
MiriamDeml <demlmiri@fim.uni-passau.de>
2020-06-01 08:31:42 +0200

    Add files via upload

---
+++ files/buildModels/model_loader.py
@@ -0,0 +1,68 @@
+import os
+import torch, torchvision
+import buildModels.models.vgg as vgg
+import buildModels.models.resnet as resnet
+import buildModels.models.densenet as densenet
+import buildModels.models.W164 as wideresnet
+import buildModels.models.R44 as resnet44
+import buildModels.models.mnist_f1 as mnist
+
+
+# Code by Li et al. (2018): https://github.com/tomgoldstein/loss-landscape/blob/master/cifar10/model_loader.py
+
+# map between model name and function
+models = {
+    'vgg9'                  : vgg.VGG9,
+    'densenet121'           : densenet.DenseNet121,
+    'resnet18'              : resnet.ResNet18,
+    'resnet18_noshort'      : resnet.ResNet18_noshort,
+    'resnet34'              : resnet.ResNet34,
+    'resnet34_noshort'      : resnet.ResNet34_noshort,
+    'resnet50'              : resnet.ResNet50,
+    'resnet50_noshort'      : resnet.ResNet50_noshort,
+    'resnet101'             : resnet.ResNet101,
+    'resnet101_noshort'     : resnet.ResNet101_noshort,
+    'resnet152'             : resnet.ResNet152,
+    'resnet152_noshort'     : resnet.ResNet152_noshort,
+    'resnet20'              : resnet.ResNet20,
+    'resnet20_noshort'      : resnet.ResNet20_noshort,
+    'resnet32_noshort'      : resnet.ResNet32_noshort,
+    'resnet44_noshort'      : resnet.ResNet44_noshort,
+    'resnet50_16_noshort'   : resnet.ResNet50_16_noshort,
+    'resnet56'              : resnet.ResNet56,
+    'resnet56_noshort'      : resnet.ResNet56_noshort,
+    'resnet110'             : resnet.ResNet110,
+    'resnet110_noshort'     : resnet.ResNet110_noshort,
+    'wrn56_2'               : resnet.WRN56_2,
+    'wrn56_2_noshort'       : resnet.WRN56_2_noshort,
+    'wrn56_4'               : resnet.WRN56_4,
+    'wrn56_4_noshort'       : resnet.WRN56_4_noshort,
+    'wrn56_8'               : resnet.WRN56_8,
+    'wrn56_8_noshort'       : resnet.WRN56_8_noshort,
+    'wrn110_2_noshort'      : resnet.WRN110_2_noshort,
+    'wrn110_4_noshort'      : resnet.WRN110_4_noshort,
+    'wrn_164'               : wideresnet.wrn164,
+    'wrn_164_100'           : wideresnet.wrn164_100,
+    'mnist_f1'              : mnist.mnist,
+    'r44'                   : resnet44.r44_C10,
+    'r44_C100'              : resnet44.r44_C100,
+}
+
+def load(model_name, model_file=None, data_parallel=False):
+    net = models[model_name]()
+    if data_parallel: # the model is saved in data paralle mode
+        net = torch.nn.DataParallel(net)
+
+    if model_file:
+        assert os.path.exists(model_file), model_file + " does not exist."
+        stored = torch.load(model_file, map_location=lambda storage, loc: storage)
+        if 'state_dict' in stored.keys():
+            net.load_state_dict(stored['state_dict'])
+        else:
+            net.load_state_dict(stored)
+
+    if data_parallel: # convert the model back to the single GPU version
+        net = net.module
+
+    net.eval()
+    return net
